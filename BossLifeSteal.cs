b0fc413581c7b0f6a222152f4a4c17698a681171
First Fractal <firstfractal512@gmail.com>
2023-04-02 14:32:46 -0500

    made the boss parts heal

--- BossLifeSteal.cs
+++ BossLifeSteal.cs
@@ -9,7 +9,8 @@ namespace BossLifeSteal
 {
     public class BossLifeSteal : Mod
     {
-        
+        public int[] BossParts = { NPCID.EaterofWorldsHead, NPCID.EaterofWorldsBody, NPCID.EaterofWorldsTail, NPCID.DungeonGuardian, NPCID.SkeletronHand, NPCID.SkeletronHead, NPCID.WallofFleshEye, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice, NPCID.PlanterasHook, NPCID.PlanterasTentacle, NPCID.GolemFistLeft, NPCID.GolemFistRight, NPCID.GolemHead, NPCID.GolemHeadFree, NPCID.CultistBossClone, NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye };
+        public int[] BossMinions = { NPCID.BlueSlime, NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.VileSpitEaterOfWorlds, NPCID.Creeper, NPCID.Bee, NPCID.BeeSmall,  NPCID.TheHungry, NPCID.TheHungryII, NPCID.QueenSlimeMinionBlue, NPCID.QueenSlimeMinionPink, NPCID.QueenSlimeMinionPurple, NPCID.Probe, NPCID.DetonatingBubble, NPCID.Sharkron, NPCID.Sharkron2, NPCID.AncientLight, NPCID.AncientDoom, NPCID.AncientCultistSquidhead, NPCID.CultistDragonHead, NPCID.CultistDragonBody1, NPCID.CultistDragonBody2, NPCID.CultistDragonBody3, NPCID.CultistDragonBody4,  NPCID.CultistDragonTail};
         public void Talk(string message, Color color)
         {
             if (Main.netMode == NetmodeID.SinglePlayer)
@@ -24,7 +25,7 @@ namespace BossLifeSteal
 
         public void LifeSteal(NPC boss, int damage)
         {
-            int heal = damage * 10;
+            int heal = damage * 5;
             boss.life += heal;
             if (boss.life > boss.lifeMax)
             {
@@ -43,6 +44,14 @@ namespace BossLifeSteal
             {
                 BLS.LifeSteal(npc, damage);
             }
+
+            foreach(int part in BLS.BossParts)
+            {
+                if (part == npc.type && npc.active)
+                {
+                    BLS.LifeSteal(npc, damage);
+                }
+            }
             base.ModifyHitPlayer(npc, target, ref damage, ref crit);
         }
     }
@@ -56,6 +65,15 @@ namespace BossLifeSteal
             {
                 BLS.LifeSteal(Main.npc[projectile.owner], damage);
             }
+
+            foreach (int part in BLS.BossParts)
+            {
+                if (Main.npc[projectile.owner].type == part && Main.npc[projectile.owner].active)
+                {
+                    BLS.LifeSteal(Main.npc[projectile.owner], damage);
+                }
+            }
+
             base.OnHitPlayer(projectile, target, damage, crit);
         }
     }
