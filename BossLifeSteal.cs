9ea5f9086d30b369f4b66b5a99765247c4926ef1
First Fractal <firstfractal512@gmail.com>
2023-04-06 20:57:49 -0500

    added in the ability for minions to life steal.

--- BossLifeSteal.cs
+++ BossLifeSteal.cs
@@ -4,13 +4,16 @@ using Terraria.Chat;
 using Terraria.ModLoader;
 using Terraria.Localization;
 using Microsoft.Xna.Framework;
+using System.Security.Cryptography.X509Certificates;
+using tModPorter;
 
 namespace BossLifeSteal
 {
     public class BossLifeSteal : Mod
     {
         public int[] BossParts = { NPCID.EaterofWorldsHead, NPCID.EaterofWorldsBody, NPCID.EaterofWorldsTail, NPCID.DungeonGuardian, NPCID.SkeletronHand, NPCID.SkeletronHead, NPCID.WallofFleshEye, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice, NPCID.PlanterasHook, NPCID.PlanterasTentacle, NPCID.GolemFistLeft, NPCID.GolemFistRight, NPCID.GolemHead, NPCID.GolemHeadFree, NPCID.CultistBossClone, NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye };
-        public int[] BossMinions = { NPCID.BlueSlime, NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.VileSpitEaterOfWorlds, NPCID.Creeper, NPCID.Bee, NPCID.BeeSmall,  NPCID.TheHungry, NPCID.TheHungryII, NPCID.QueenSlimeMinionBlue, NPCID.QueenSlimeMinionPink, NPCID.QueenSlimeMinionPurple, NPCID.Probe, NPCID.DetonatingBubble, NPCID.Sharkron, NPCID.Sharkron2, NPCID.AncientLight, NPCID.AncientDoom, NPCID.AncientCultistSquidhead, NPCID.CultistDragonHead, NPCID.CultistDragonBody1, NPCID.CultistDragonBody2, NPCID.CultistDragonBody3, NPCID.CultistDragonBody4,  NPCID.CultistDragonTail};
+        public int[] BossMinions = { NPCID.BlueSlime, NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.Creeper, NPCID.Bee, NPCID.BeeSmall,  NPCID.TheHungry, NPCID.TheHungryII, NPCID.QueenSlimeMinionBlue, NPCID.QueenSlimeMinionPink, NPCID.QueenSlimeMinionPurple, NPCID.Probe, NPCID.DetonatingBubble, NPCID.Sharkron, NPCID.Sharkron2, NPCID.AncientCultistSquidhead, NPCID.CultistDragonHead, NPCID.CultistDragonBody1, NPCID.CultistDragonBody2, NPCID.CultistDragonBody3, NPCID.CultistDragonBody4,  NPCID.CultistDragonTail};
+        public bool boss = false;
         public void Talk(string message, Color color)
         {
             if (Main.netMode == NetmodeID.SinglePlayer)
@@ -42,6 +45,27 @@ namespace BossLifeSteal
     public class BossLifeStealNPC : GlobalNPC
     {
         public static BossLifeSteal BLS = new BossLifeSteal();
+        public override bool PreAI(NPC npc)
+        {
+            BLS.boss = false;
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                if (Main.npc[i].boss && Main.npc[i].active == true)
+                {
+                    BLS.boss = true;
+                }
+
+                foreach (int bossPart in BLS.BossParts)
+                {
+                    if (Main.npc[i].type == bossPart && Main.npc[i].active == true)
+                    {
+                        BLS.boss = true;
+                    }
+                }
+            }
+            return base.PreAI(npc);
+        }
+
         public override void ModifyHitPlayer(NPC npc, Player target, ref int damage, ref bool crit)
         {
             if (npc.boss && npc.active)
@@ -54,6 +78,17 @@ namespace BossLifeSteal
                 if (part == npc.type && npc.active)
                 {
                     BLS.LifeSteal(npc, damage);
+                }
+            }
+
+            if (BLS.boss && BossLifeStealConfig.Instance.MinionsLifeSteal)
+            {
+                foreach (int minion in BLS.BossMinions)
+                {
+                    if (minion == npc.type && npc.active)
+                    {
+                        BLS.LifeSteal(npc, damage);
+                    }
                 }
             }
             base.ModifyHitPlayer(npc, target, ref damage, ref crit);
@@ -75,6 +110,17 @@ namespace BossLifeSteal
                 if (Main.npc[projectile.owner].type == part && Main.npc[projectile.owner].active)
                 {
                     BLS.LifeSteal(Main.npc[projectile.owner], damage);
+                }
+            }
+
+            if (BLS.boss && BossLifeStealConfig.Instance.MinionsLifeSteal) 
+            {
+                foreach (int minion in BLS.BossMinions)
+                {
+                    if (Main.npc[projectile.owner].type == minion && Main.npc[projectile.owner].active)
+                    {
+                        BLS.LifeSteal(Main.npc[projectile.owner], damage);
+                    }
                 }
             }
 
--- BossLifeStealConfig.cs
+++ BossLifeStealConfig.cs
@@ -22,11 +22,15 @@ namespace BossLifeSteal
         [Label("$Mods.BossLifeSteal.Config.LifeStealMulti.Label")]
         [Tooltip("$Mods.BossLifeSteal.Config.LifeStealMulti.Tooltip")]
         [Range(0, 20)]
-        //[Increment(10)]
         [Slider]
         [DrawTicks()]
         [DefaultValue(3)]
         public int LifeStealMulti;
+
+        [Label("$Mods.BossLifeSteal.Config.MinionsLifeSteal.Label")]
+        [Tooltip("$Mods.BossLifeSteal.Config.MinionsLifeSteal.Tooltip")]
+        [DefaultValue(true)]
+        public bool MinionsLifeSteal;
 
         [Label("$Mods.BossLifeSteal.Config.EnableChat.Label")]
         [Tooltip("$Mods.BossLifeSteal.Config.EnableChat.Tooltip")]
